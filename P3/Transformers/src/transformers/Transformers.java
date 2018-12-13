b3863bec90d7fddfdf56a7cade74e5c445fa3e7c
juang3 <juang3@correo.ugr.es>
2018-12-13 11:48:16 +0100

    Creados más de un agente vehículo para ver la comunicación implementada has este momento.

--- P3/Transformers/src/transformers/Transformers.java
+++ P3/Transformers/src/transformers/Transformers.java
@@ -7,6 +7,7 @@ package transformers;
 
 import es.upv.dsic.gti_ia.core.AgentID;
 import es.upv.dsic.gti_ia.core.AgentsConnection;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,10 +21,7 @@ public class Transformers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         try{
-            AgentID id_burocrata = new AgentID("Optimus_Prime00");
-            //AgentID destinatario = new AgentID("Explorador1");
-            AgentID id_v = new AgentID("vehiculo00");
+         try{    
             String nombreServidor = "Girtab";
             String nombreMapa = "map1";
             AgentID id_servidor = new AgentID(nombreServidor);
@@ -32,14 +30,43 @@ public class Transformers {
             AgentsConnection.connect("isg2.ugr.es", 6000, nombreServidor, "Geminis", "France", false);
             System.out.println("Conectado a isg2.ugr.es");
             
-            // Crear agente
-            boolean informa = true;
-            Burocrata optimusPrime = new Burocrata(id_burocrata, nombreServidor, id_v, nombreMapa, informa);
-            Vehiculo v = new Vehiculo(id_v, id_servidor, id_burocrata, informa);
             
-            // Despiertas agente.
+            // Creando agentID para los agentes
+            AgentID id_burocrata = new AgentID("Optimus_Prime000");
+            AgentID id_v1 = new AgentID("vehiculo000");
+            AgentID id_v2 = new AgentID("vehiculo111");
+            AgentID id_v3 = new AgentID("vehiculo222");
+            AgentID id_v4 = new AgentID("vehiculo333");
+            
+            ArrayList<AgentID> vehiculos = new ArrayList<>();
+            vehiculos.add(id_v1);
+            vehiculos.add(id_v2);
+            vehiculos.add(id_v3);
+            vehiculos.add(id_v4);
+            
+             System.out.println("\n VEHICULOS: "+ vehiculos.toString()
+             + " cuyo tamaño es: "+ vehiculos.size());
+             
+            // Creando a los agentes y activando el verboseo
+            boolean informa = true;
+            
+            Burocrata optimusPrime = new Burocrata(id_burocrata, nombreServidor,
+                    vehiculos, nombreMapa, informa);
+            
+            Vehiculo v1 = new Vehiculo(id_v1, id_servidor, id_burocrata, informa);
+            Vehiculo v2 = new Vehiculo(id_v2, id_servidor, id_burocrata, informa);
+            Vehiculo v3 = new Vehiculo(id_v3, id_servidor, id_burocrata, informa);
+            Vehiculo v4 = new Vehiculo(id_v4, id_servidor, id_burocrata, informa);
+            /** Despertando a los agentes.
+             * @Nota: Es necesario que los vehículos despierten antes porque
+             * esperan recbir del agente burócrata el conversationID.
+             */
+            v1.start();
+            v2.start();
+            v3.start();
+            v4.start();
             optimusPrime.start();
-            v.start();
+            
         }
         catch(Exception ex) {
             System.out.println("Excepción " + ex.toString());
